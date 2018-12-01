d2f037dcb073a0166d5c39bb34c36de4ccd65efc
Stanislav Nedelchev <stan@stannedelchev.net>
2018-12-01 13:50:23 +0200

    Solve Day 1

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventOfCode2018
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var problems = new[] { new Day1.Problem() };
+
+            foreach (var problem in problems)
+            {
+                var day = problem.GetType().Namespace.Split(".", StringSplitOptions.RemoveEmptyEntries).Last();
+                var result1 = problem.Part1(File.ReadAllLines($"{day}\\input.txt"));
+                var result2 = problem.Part2(File.ReadAllLines($"{day}\\input.txt"));
+                Console.WriteLine($"{day}.1 - {result1}");
+                Console.WriteLine($"{day}.2 - {result2}");
+            }
         }
     }
 }
