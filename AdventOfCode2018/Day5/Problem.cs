74d8503b6f4bc4c4e42fb16d6c5a03c52380271b
Stanislav Nedelchev <stan@stannedelchev.net>
2018-12-06 23:28:02 +0200

    Optimize Day 5

--- AdventOfCode2018/Day5/Problem.cs
+++ AdventOfCode2018/Day5/Problem.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace AdventOfCode2018.Day5
 {
@@ -7,17 +7,17 @@ namespace AdventOfCode2018.Day5
         public string Part1(string[] input)
         {
             var polymer = this.ReactPolymer(input[0]);
-            return polymer.Length.ToString();
+            return polymer.ToString();
         }
 
         public string Part2(string[] input)
         {
             var polymer = input[0];
             var minLen = int.MaxValue;
-            foreach(var ch in "abcdefghijklmnopqrstuvwxyz")
+            foreach (var ch in "abcdefghijklmnopqrstuvwxyz")
             {
-                var len = this.ReactPolymer(polymer.Replace(ch.ToString(), "").Replace(char.ToUpper(ch).ToString(), "")).Length;
-                if(len < minLen)
+                var len = this.ReactPolymer(polymer.Replace(ch.ToString(), "").Replace(char.ToUpper(ch).ToString(), ""));
+                if (len < minLen)
                 {
                     minLen = len;
                 }
@@ -26,20 +26,39 @@ namespace AdventOfCode2018.Day5
             return minLen.ToString();
         }
 
-        private string ReactPolymer(string polymer)
+        private int ReactPolymer(string polymer)
         {
-            for (var i = 0; i < polymer.Length - 1; i++)
+            var polymerLength = polymer.Length;
+            var stack = new Stack<char>();
+
+            for(int polymerIndex = 0; polymerIndex < polymerLength; polymerIndex++)
             {
-                if (char.ToLower(polymer[i]) == char.ToLower(polymer[i + 1])
-                    && (char.IsUpper(polymer[i]) && char.IsLower(polymer[i + 1])
-                        || char.IsLower(polymer[i]) && char.IsUpper(polymer[i + 1])))
+                var nextChar = polymer[polymerIndex];
+
+                if (stack.Count == 0)
                 {
-                    polymer = polymer.Remove(i, 2);
-                    i = Math.Max(-1, i - 2);
+                    stack.Push(nextChar);
+                    continue;
                 }
+
+                var peekedChar = stack.Peek();
+                if (peekedChar == nextChar)
+                {
+                    stack.Push(nextChar);
+                    continue;
+                }
+
+                var charDifference = nextChar - peekedChar;
+                if (charDifference == 32 || charDifference == -32)
+                {
+                    stack.Pop();
+                    continue;
+                }
+
+                stack.Push(nextChar);
             }
 
-            return polymer;
+            return stack.Count;
         }
     }
 }
