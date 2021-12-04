83d56d08c8bb85acc3501d811edbe6d66578443c
Stanislav Nedelchev <stan@stannedelchev.net>
2021-12-04 09:45:29 +0200

    WIP

---
+++ AdventOfCode2021/Day04/Problem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Shared;
+
+namespace AdventOfCode2021.Day04
+{
+    internal class Problem : IProblem
+    {
+        public string Part1(string[] input)
+        {
+            var draws = input[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var boards = new List<List<List<int>>>();
+            var board = new List<List<int>>();
+            foreach (var line in input.Skip(2))
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    boards.Add(board);
+                    board = new List<List<int>>();
+                    continue;
+                }
+
+                var row = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                board.Add(row);
+            }
+            boards.Add(board);
+
+            foreach(var number in draws)
+            {
+            }
+
+            return "";
+        }
+
+        public string Part2(string[] input)
+        {
+            return "";
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -4,7 +4,8 @@ var problems = new IProblem[] {
     new AdventOfCode2021.Day01.Problem(),
     new AdventOfCode2021.Day02.Problem(),
     new AdventOfCode2021.Day03.Problem(),
+    new AdventOfCode2021.Day04.Problem(),
 };
 
-var runner = new ProblemRunner(problems, 100_000, true, true);
+var runner = new ProblemRunner(problems, 100, true, true);
 runner.Run();
